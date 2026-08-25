427344c04f23022faa2620a3188485f782c1097b
salombro112 <112fasc1st@gmail.com>
2026-08-25 15:32:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Asosiy dastur kodi bu yerda, Source 6 dan olingan tuzilma bo'yicha.
+const CONFIG = {
+  BOT_TOKEN: "YOUR_BOT_TOKEN",
+  CHAT_ID: "YOUR_CHAT_ID",
+  SMS: { API_URL: "", API_KEY: "", DEVICE_ID: "", CODE_TTL_SEC: 120 },
+  USD_TO_SUM: 12600,
+  FIREBASE: { apiKey: "YOUR_FIREBASE_API_KEY", authDomain: "", databaseURL: "", projectId: "" },
+};
+// Va boshqa barcha funksiyalar...
+console.log("App initialized");
